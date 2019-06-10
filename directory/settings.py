8b09f1ba59944f5021edb4f0b911ad8c56e6c9ef
abby-shabi <abbyshabi@gmail.com>
2019-06-10 22:11:09 +0300

    changed db from postgres to sqlite

--- directory/settings.py
+++ directory/settings.py
@@ -82,10 +82,8 @@ WSGI_APPLICATION = 'directory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mail',
-        'USER':'AbbyShabi',
-        'PASSWORD':'dammy',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
